9270b01235d3f82b5ebffee11ed61c95800b214b
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2024-12-16 19:59:53 +0100

    refactor: Inline _loop method into run method for simplified agent workflow

--- modules/agent_service.py
+++ modules/agent_service.py
@@ -10,18 +10,6 @@ class AgentService:
     async def run(self, parent_trace=None) -> str:
         self._store_message(self.state.messages[-1]['content'], "user")
 
-        await self._loop(parent_trace)
-        final_answer = await self.planning_service.create_final_answer(
-            self.state, self.state.messages, parent_trace
-        )
-
-        self._store_message(final_answer, "assistant")
-        return final_answer
-    
-    def _store_message(self, message: str, role: str):
-        self.db_service.store_message(self.state.conversation_uuid, role, message)
-
-    async def _loop(self, parent_trace=None):
         while self.state.config["current_step"] < self.state.config["max_steps"]:
             plan_result = await self.planning_service.create_plan(
                 self.state, self.state.messages, parent_trace
@@ -32,4 +20,14 @@ class AgentService:
                 
             await self.execution_service.execute_plan(plan_result, self.state, parent_trace)
             self.state.config["current_step"] += 1
+
+        final_answer = await self.planning_service.create_final_answer(
+            self.state, self.state.messages, parent_trace
+        )
+
+        self._store_message(final_answer, "assistant")
+        return final_answer
+    
+    def _store_message(self, message: str, role: str):
+        self.db_service.store_message(self.state.conversation_uuid, role, message)
 
