c52c0a9d34d50341f1390f040076fb85eda56564
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2024-12-31 14:27:43 +0100

    simplify

--- ynab/service.py
+++ ynab/service.py
@@ -1,6 +1,6 @@
-from typing import Dict
+from typing import Dict, Any
 
-from types.action import ActionResult
+from models.action import ActionResult
 
 
 async def ynab_tool(action, params: Dict[str, Any], trace) -> ActionResult:
