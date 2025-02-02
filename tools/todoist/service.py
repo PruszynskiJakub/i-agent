71f1ce158225feb77054aeb7ef703fc7072003a9
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2025-02-02 22:10:16 +0100

    Improve

--- tools/todoist/service.py
+++ tools/todoist/service.py
@@ -1,13 +1,13 @@
 from typing import List
 
 from models.document import Document
-from utils.document import create_error_document
 from tools.todoist.internal.add_tasks import add_tasks
 from tools.todoist.internal.complete_tasks import complete_tasks
 from tools.todoist.internal.get_projects import get_projects
+from tools.todoist.internal.move_tasks import move_tasks
 from tools.todoist.internal.search_tasks import search_tasks
 from tools.todoist.internal.update_tasks import update_tasks
-from tools.todoist.internal.move_tasks import move_tasks
+from utils.document import create_error_document
 
 
 async def execute_todoist(action: str, params: dict[str, any], span) -> List[Document]:
@@ -31,14 +31,18 @@ async def execute_todoist(action: str, params: dict[str, any], span) -> List[Doc
             doc = await move_tasks(params, span)
             return [doc]
         else:
-            return [create_error_document(
-                Exception(f"Unknown action: {action}"),
-                f"Todoist service received unknown action",
+            return [
+                create_error_document(
+                    Exception(f"Unknown action: {action}"),
+                    f"Todoist service received unknown action",
+                    params.get("conversation_uuid", "")
+                )
+            ]
+    except Exception as exception:
+        return [
+            create_error_document(
+                exception,
+                f"Error executing Todoist action: {action}",
                 params.get("conversation_uuid", "")
-            )]
-    except Exception as e:
-        return [create_error_document(
-            e,
-            f"Error executing Todoist action: {action}",
-            params.get("conversation_uuid", "")
-        )]
+            )
+        ]
