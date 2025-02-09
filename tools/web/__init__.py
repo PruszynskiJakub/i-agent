e374cb305d1cda1f941e288275eae7fe97bad0d4
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2025-02-09 18:19:18 +0100

    Improve

--- tools/web/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-from firecrawl import FirecrawlApp
-
-load_dotenv()
-firecrawl_client =  FirecrawlApp(api_key=os.environ["FIRECRAWL_API_KEY"])
--- tools/web/types.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class WebContent:
-    """Content fetched from a web URL"""
-    url: str
-    content: str
-    type: str  # 'md' or 'html'
